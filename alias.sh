725f5b49b8a44d3c73b043ae10eb012b6a9fdabe
lig001 <lig001@cmrh.com>
2019-03-09 14:39:14 +0800

    init

---
+++ alias.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+alias gc='git checkout'
+alias gcm='git checkout master'
+
+alias gll='git pull'
+alias gsh='git push'
+alias gb='git branch'
+alias gs='git status'
+
+alias gch='git checkout -- .'
+alias gre='git reset HEAD'
+
+alias gp1='git stash push -m "temp"'
+alias gp2='git stash pop'
